f4d0ef72f049da73fb363b896bc0dafb5db1d341
llrs <lluis.revilla@gmail.com>
2018-07-02 15:46:32 +0200

    Update the tests

--- tests/testthat/test_sgcca.R
+++ tests/testthat/test_sgcca.R
@@ -1,20 +1,20 @@
-library("RGCCA")
-library("gliomaData")
-data("ge_cgh_locIGR")
 
 context("Testing sgcca")
 
 
-A <- ge_cgh_locIGR$multiblocks
-Loc <- factor(ge_cgh_locIGR$y) ; levels(Loc) <- colnames(ge_cgh_locIGR$multiblocks$y)
-C <-  matrix(c(0, 0, 1, 0, 0, 1, 1, 1, 0), 3, 3)
-tau <- c(1, 1, 0)
 
 
 
 test_that("sgcca output does what it is expected", {
+  library("gliomaData")
+  data("ge_cgh_locIGR")
+  A <- ge_cgh_locIGR$multiblocks
+  Loc <- factor(ge_cgh_locIGR$y) ; levels(Loc) <- colnames(ge_cgh_locIGR$multiblocks$y)
+  C <-  matrix(c(0, 0, 1, 0, 0, 1, 1, 1, 0), 3, 3)
+  tau <- c(1, 1, 0)
+
   result.sgcca <- sgcca(A, C, c1 = c(.071,.2, 1), ncomp = c(2, 2, 1),
-                       scheme = "centroid", verbose = FALSE)
+                        scheme = "centroid", verbose = FALSE)
 
   expect_equal(result.sgcca$C, C)
   expect_equal(result.sgcca$ncomp, c(2L, 2L, 1L))
