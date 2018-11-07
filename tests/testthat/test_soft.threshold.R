cb18eb6c55d6a6ef37b20a8ff5509ff2c9d57251
Lluís Revilla <lluis.revilla@gmail.com>
2018-11-07 22:54:40 +0100

    Add more tests

--- tests/testthat/test_soft.threshold.R
+++ tests/testthat/test_soft.threshold.R
@@ -1,6 +1,6 @@
-context("soft.threshold")
+context("test_soft.threshold")
 
-test_that("works as is", {
+test_that("soft.threshold works", {
   x <- rnorm(10)
   out <- soft.threshold(x, 0.5)
   expect_equal(out, c(0, 0, 0, 0, 0, 0, 0, 1.34653177497057e-10, 0, 0))
