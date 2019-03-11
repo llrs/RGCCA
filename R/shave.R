5dc762546b7af150e0177ae84cda530086b32ef5
llrs <lluis.revilla@gmail.com>
2019-03-11 11:35:17 +0100

    Still failing the check! but I don't know how to create a method for this type of object

--- R/shave.R
+++ R/shave.R
@@ -3,14 +3,17 @@ shave <- function(m, nbcomp) {
   UseMethod("shave")
 }
 
-shave.list <- function(m, nbcomp) {
-  mapply(shaving, m, nbcomp, SIMPLIFY = FALSE)
-}
-
 shaving <- function(m, nbcomp) {
   UseMethod("shaving")
 }
 
+shave.list <- function(m, nbcomp) {
+  mapply(shaving, m, nbcomp, SIMPLIFY = FALSE)
+}
+
+shaving.defult <- function(m, nbcomp) {
+  m[seq_len(nbcomp)]
+}
 shaving.matrix <- function(m, nbcomp) {
   m[, seq_len(nbcomp), drop = FALSE]
 }
@@ -20,5 +23,8 @@ shaving.vector <- function(m, nbcomp) {
 }
 
 shaving.numeric <- function(m, nbcomp) {
+  m[seq_len(nbcomp)]
+}
+shaving.double <- function(m, nbcomp) {
   m[seq_len(nbcomp)]
 }
