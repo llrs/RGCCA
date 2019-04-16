937bb994b5fae6b280c88083a49f17b7f33f49a2
llrs <lluis.revilla@gmail.com>
2019-04-16 15:05:22 +0200

    Add more tests on tau.estimate

--- tests/testthat/test-tau.estimate.R
+++ tests/testthat/test-tau.estimate.R
@@ -3,4 +3,13 @@ context("tau.estimate")
 test_that("tau.estimate", {
   data("Russett")
   expect_equal(tau.estimate(Russett), 0.114279478757313)
+
+})
+test_that("implementations are coherent", {
+  data(Russett)
+  set.seed(45791)
+  X_agric <- as.matrix(Russett[, c("gini", "farm", "rent")])
+  s <- scale2_(X_agric, bias = TRUE)
+  t1 <- tau.estimate(s)
+  expect_true(t1 != 1)
 })
