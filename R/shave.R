2f3dc1f5b10d7ff8ff491206dc0c1334d913e665
Jim Hester <james.f.hester@gmail.com>
2019-04-26 08:46:10 -0400

    Register the shaving methods
    Fixes https://github.com/r-lib/covr/issues/362

--- R/shave.R
+++ R/shave.R
@@ -7,24 +7,32 @@ shaving <- function(m, nbcomp) {
   UseMethod("shaving")
 }
 
+#' @export
 shave.list <- function(m, nbcomp) {
   mapply(shaving, m, nbcomp, SIMPLIFY = FALSE)
 }
 
+#' @export
 shaving.defult <- function(m, nbcomp) {
   m[seq_len(nbcomp)]
 }
+
+#' @export
 shaving.matrix <- function(m, nbcomp) {
   m[, seq_len(nbcomp), drop = FALSE]
 }
 
+#' @export
 shaving.vector <- function(m, nbcomp) {
   m[seq_len(nbcomp)]
 }
 
+#' @export
 shaving.numeric <- function(m, nbcomp) {
   m[seq_len(nbcomp)]
 }
+
+#' @export
 shaving.double <- function(m, nbcomp) {
   m[seq_len(nbcomp)]
 }
