8b06dc0eee6a2aca9fd9350cdcade053d8d5cbe6
llrs <lluis.revilla@gmail.com>
2021-02-26 13:12:38 +0100

    Further recording of versions

--- tests/testthat/test-soft.R
+++ tests/testthat/test-soft.R
@@ -1,10 +1,7 @@
 set.seed(45)
-x <- rnorm(10)
-d <- rnorm(10)
+x <- rnorm(40000)
+d <- rnorm(40000)
 
-
-x2 <- rnorm(15702)
-d2 <- rnorm(15702)
 test_that("soft works", {
   out <- soft(x, d)
   expect_equal(out, c(1.47121790923518, -0.487351414498001, 0, 0,
@@ -13,7 +10,9 @@ test_that("soft works", {
                       -1.88260095097369))
 })
 
-test_that("equivalent (but slower)",  {
+test_that("equivalent (but slower on big numbers)",  {
   out <- sign(x) * ifelse(abs(x) - d > 0, abs(x) - d, 0)
+  expect_equal(out, soft(x, d))
+  out <- sign(x) * do.call(pmax, list(0, abs(x) - d))
   expect_equal(out, soft(x, d))
 })
